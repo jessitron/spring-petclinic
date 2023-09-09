dc145309b4a39459b331e9fcdcc92cf6b3dc24a3
Jessica Kerr <jessitron@gmail.com>
2023-09-09 17:28:14 -0500

    it didn't like the -D early

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
+# sadly this does not work, it runs the integration tests
 ./mvnw package -Dskip.it=true
 
 mkdir -p target/extracted
-java -Djarmode=layertools -jar target/*.jar extract --destination target/extracted
+java -jar -Djarmode=layertools target/*.jar extract --destination target/extracted
 docker build -t demo-otel-operator/petclinic .
